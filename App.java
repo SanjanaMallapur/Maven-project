9806e8922bf33534c9b5650b50f917789b32c6e3
Tanushree-22 <tanushreempujar@gmail.com>
2025-04-08 08:51:32 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+package comm.explore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    
+
+
+    public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+    }
+  }
+}
